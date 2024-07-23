475aba9d31b699aed1bf5f890012e6012d9f4f46
Dmitry Shamko <proffery@gmail.com>
2024-07-23 19:08:35 +0300

    Fonts, Global CSS, MUI installation

---
+++ src/styles/fonts.ts
@@ -0,0 +1,15 @@
+import { Audiowide, Creepster } from 'next/font/google';
+
+export const audiowide = Audiowide({
+    weight: ['400'],
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-audiowide'
+});
+
+export const creepster  = Creepster({
+    weight: ['400'],
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-creepster'
+});
